3513debde35e13eab97a2cbc67ca478411e4cf59
Dylan Del Sontro <dylandelsontro@gmail.com>
2025-02-21 15:45:49 -0500

    c: Point and Line classes implemented; sample line  drawing code shown in updated main.py.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    li = Line(Point(0, 50), Point(600, 200))
+    win.draw_line(li, "black")
     win.wait_for_close()
 
 
