f49a976d3ad051eb006f3d2d5bf775427b380fb5
Dylan Del Sontro <dylandelsontro@gmail.com>
2025-02-21 18:04:43 -0500

    F: Created maze.py and its methods; sample code found in updated main.py. WARNING: the generated maze looks to be incorrectly rending...

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600) #Window viewport size (x,y)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    cel1 = Cell(win)
-    cel1.has_left_wall = False
-    cel1.draw(50, 50, 100, 100)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
-    cel2 = Cell(win)
-    cel2.has_left_wall = False
-    cel2.has_bottom_wall = False
-    cel2.draw(100, 50, 150, 100)
+    
 
-    cel1.draw_move(cel2)
-
-    cel3 = Cell(win)
-    cel3.has_top_wall = False
-    cel3.has_right_wall = False
-    cel3.draw(100, 100, 150, 150)
-
-    cel2.draw_move(cel3)
-
-    cel4 = Cell(win)
-    cel4.has_top_wall = False
-    cel4.draw(150, 100, 200, 150)
-
-    cel3.draw_move(cel4, True)
 
     win.wait_for_close()
 
