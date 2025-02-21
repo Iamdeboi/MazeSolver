808cdd0fbedbf1c5939adf8398dc457963132354
Dylan Del Sontro <dylandelsontro@gmail.com>
2025-02-21 17:13:17 -0500

    E: Cell class draw_move() method created; draws line through cells given cell of origin and its dir  as to_cell. Sample code found in updated main.py.

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from cell import Cell
 def main():
     win = Window(800, 600) #Window viewport size (x,y)
 
-    cel = Cell(win)
-    cel.has_left_wall = False
-    cel.draw(50, 50, 100, 100)
+    cel1 = Cell(win)
+    cel1.has_left_wall = False
+    cel1.draw(50, 50, 100, 100)
 
-    cel = Cell(win)
-    cel.has_right_wall = False
-    cel.draw(125, 125, 200, 200)
+    cel2 = Cell(win)
+    cel2.has_left_wall = False
+    cel2.has_bottom_wall = False
+    cel2.draw(100, 50, 150, 100)
 
-    cel = Cell(win)
-    cel.has_bottom_wall = False
-    cel.draw(225, 225, 250, 250)
+    cel1.draw_move(cel2)
 
-    cel = Cell(win)
-    cel.has_top_wall = False
-    cel.draw(300, 300, 500, 500)
+    cel3 = Cell(win)
+    cel3.has_top_wall = False
+    cel3.has_right_wall = False
+    cel3.draw(100, 100, 150, 150)
+
+    cel2.draw_move(cel3)
+
+    cel4 = Cell(win)
+    cel4.has_top_wall = False
+    cel4.draw(150, 100, 200, 150)
+
+    cel3.draw_move(cel4, True)
 
     win.wait_for_close()
 
