b8256e2946b4b1fea77b31549c18e3a187500acb
Dylan Del Sontro <dylandelsontro@gmail.com>
2025-02-21 19:44:07 -0500

    I: Added methods for solving the maze by drawing lines through the modified maze grid and printing a  message depending on if the maze is solvable or not.

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from graphics import Window
 from maze import Maze
+import sys
 
 def main():
     num_rows = 12
     num_cols = 16
-    margin = 50
+    margin = 0
     screen_x = 800
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
 
+    sys.setrecursionlimit(10000)
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
-
-    
-
-
+    print("maze created!")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("The maze cannot be solved...")
+    else:
+        print("The maze has been solved!")
     win.wait_for_close()
-
+    
 
 main()
